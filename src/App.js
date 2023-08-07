a8797ce2c2fef46dc511a5e192b65a61a9b51b21
Sourav0809 <spathak7431@gmail.com>
2023-08-07 21:21:14 +0530

    deliverable done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -7,19 +7,34 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([])
   const [loader, setLoader] = useState(false)
+  const [error, setError] = useState(null)
+  const timerRef = useRef()
 
+  // useEffect(() => {
+  //   return () => {
+  //     clearInterval(timerRef.current)
+  //   }
+  // }, [error])
 
+  const retryFunction = () => {
+    timerRef.current = setTimeout(() => {
+      fetchMovieHandeler()
+    }, 2000);
+  }
 
   //fetch handeler function to fetched some data
-
-  const fetchMovieHandeler = async () => {
+  async function fetchMovieHandeler() {
     setLoader(true)
     try {
-      const fetchedData = await fetch('https://swapi.dev/api/films/').then((Response) => {
-        return Response.json()
 
-      })
-      const transMovies = fetchedData.results.map((data) => {
+      const fetchedData = await fetch('https://swapi.dev/api/film/')
+      // if the api calls fails 
+      if (!fetchedData.ok) {
+        throw new Error("Some Thing went wrong ! Retrying...")
+      }
+
+      const data = await fetchedData.json()
+      const transMovies = data.results.map((data) => {
         return {
           id: data.episode_id,
           title: data.title,
@@ -28,25 +43,34 @@ function App() {
 
         }
       })
-      setLoader(false)
+      clearTimeout(timerRef.current)
       setMovies(transMovies)
 
     } catch (error) {
       console.log(error)
-    }
-  }
+      setError(error.message)
+      retryFunction()
 
+    }
+    setLoader(false)
+
+  }
+  //loader screen 
   const loaderScreen = <div><img src='https://media.tenor.com/wpSo-8CrXqUAAAAi/loading-loading-forever.gif' alt='gif img' height={100} width={100} /></div>
 
   return (
-    <React.Fragment>
+    <>
       <section>
         <button onClick={fetchMovieHandeler}>Fetch Movies</button>
+
       </section>
       <section>
+
         {loader ? loaderScreen : <MoviesList movies={movies} />}
+        {!loader && error && <p>{error}</p>}
+        {error && <button onClick={() => { clearTimeout(timerRef.current) }}>Stop</button>}
       </section>
-    </React.Fragment>
+    </>
   );
 }
 
